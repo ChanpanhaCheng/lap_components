74faf78bb0387551d1008a93c41d701cdaf55518
ChanpanhaCheng <chanpanhacheng@gmail.com>
2025-03-02 15:24:05 +0700

    finished

---
+++ Start code/exercise-2/myapp/src/App.jsx
@@ -0,0 +1,15 @@
+import Main from './component/Main'
+import Header from './component/Header'
+import Footer from './component/Footer'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
